75049b94e7bb12f368343eee852fb2c39db4652c
FlameFox <62987656+Userbot007@users.noreply.github.com>
2020-11-03 21:39:01 +0530

    Update __main__.py
    Huh changed something

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -53,15 +53,12 @@ def get_readable_time(seconds: int) -> str:
 
 PM_START_TEXT = """
 Hi {}, my name is {}! 
-I am an Anime themed group management bot.
-Build by weebs for weebs, I specialize in managing anime and similar themed groups.
-You can find my list of available commands with /help.
+I can handle your groups very well cause I have a hot figure, and u bois love bhabhi!!.
 """
 
 HELP_STRINGS = """
 Hey there! My name is *{}*.
-I'm a Hero For Fun and help admins manage their groups with One Punch! Have a look at the following for an idea of some of \
-the things I can help you with.
+Do you want a B job ??
 
 *Main* commands available:
  • /help: PM's you this message.
@@ -78,12 +75,10 @@ And the following:
     dispatcher.bot.first_name, ""
     if not ALLOW_EXCL else "\nAll commands can either be used with / or !.\n")
 
-SAITAMA_IMG = "https://telegra.ph/file/46e6d9dfcb3eb9eae95d9.jpg"
+SAITAMA_IMG = "https://telegra.ph/file/4efec741761b6c4f1a31e.jpg"
 
 DONATE_STRING = """Heya, glad to hear you want to donate!
-Saitama is hosted on one of Kaizoku's Servers and doesn't require any donations as of now but \
-You can donate to the original writer of the Base code, Paul
-There are two ways of supporting him; [PayPal](paypal.me/PaulSonOfLars), or [Monzo](monzo.me/paulnionvestergaardlarsen)."""
+I'm running on heroku and don't need any donation."""
 
 IMPORTED = {}
 MIGRATEABLE = []
@@ -207,20 +202,20 @@ def start(update: Update, context: CallbackContext):
                     ],
                      [
                          InlineKeyboardButton(
-                             text="🚑 Support Group",
-                             url=f"https://t.me/{SUPPORT_CHAT}"),
+                             text="🚑 DoNOT press here",
+                             url=f"https://dotesports.com/culture/news/the-origin-of-press-f-to-pay-respects"),
                          InlineKeyboardButton(
-                             text="🔔 Updates Channel",
-                             url="https://t.me/OnePunchUpdates")
+                             text="🔔 Subscribe to this channel if you love me",
+                             url="https://www.youtube.com/channel/UC2MtpbwwzK4vVBSnsYDrXKA")
                      ],
                      [
                          InlineKeyboardButton(
-                             text="🧾 Getting started guide",
-                             url="https://t.me/OnePunchUpdates/29")
+                             text="🧾 Click here to see my private photos",
+                             url="https://i.imgur.com/ihFa5bb.jpg")
                      ],
                      [
                          InlineKeyboardButton(
-                             text="🗄 Source code",
+                             text="🗄 Original Source code",
                              url="https://github.com/AnimeKaizoku/SaitamaRobot")
                      ]]))
     else:
