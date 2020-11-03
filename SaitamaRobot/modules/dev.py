e7a9b4c162230738b1ded686d8a9bd4afcd322b8
FlameFox <62987656+Userbot007@users.noreply.github.com>
2020-11-03 21:48:53 +0530

    Update dev.py
    Nap

--- SaitamaRobot/modules/dev.py
+++ SaitamaRobot/modules/dev.py
@@ -49,7 +49,7 @@ def gitpull(update: Update, context: CallbackContext):
 @dev_plus
 def restart(update: Update, context: CallbackContext):
     update.effective_message.reply_text(
-        "Starting a new instance and shutting down this one")
+        "Ok I'm gonna take a nap and will come later")
 
     os.system('restart.bat')
     os.execv('start.bat', sys.argv)
